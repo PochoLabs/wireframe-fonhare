8fdcc4117334008a710d3a86dd23091f2fc8dd9a
Celso Mireles <celso@digitalstrategy.tips>
2015-12-18 12:49:43 -0700

    Separated navigation

--- js/main.js
+++ js/main.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
 	$('.carousel').carousel({
         interval: 5000 //changes the speed
     });
+
+	// for test hosting
+	var domain = $(location).attr('hostname');
+	if(domain === 'localhost'){
+		$('a.serving').each(function(){
+			var newLink = $(this).attr('href').slice(8);
+			$(this).attr('href', newLink);
+		});
+	}
 });
